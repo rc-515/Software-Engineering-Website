b362312966f8b5a9192f25b32442e7bedb393593
alexglotzer <aglotzer@wesleyan.edu>
2025-03-06 23:28:57 -0500

    Update add_match.php

--- add_match.php
+++ add_match.php
@@ -47,7 +47,8 @@ if ($result->num_rows > 0) {
         echo "<form method='POST' action=''>
                 <input type='hidden' name='opponent_id' value='" . $row["id"] . "'>
                 <input type='hidden' name='fight_date' value='" . $fight_date . "'>
-                <button type='submit' name='schedule_match'>Schedule</button>
+                <button type='submit' name='schedule_match' onclick="alert('Schedule button clicked');">Schedule</button>
+
               </form><br><br>";
     }
 } else {
