5e083a3f87a23628be09ca7a00479f10e247b10b
alexglotzer <aglotzer@wesleyan.edu>
2025-04-10 10:38:55 -0400

    Update App.js

--- fightclub-app/App.js
+++ fightclub-app/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-console.log("App.js rendered");
+
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
